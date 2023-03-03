cf8174eec0fd682c06a11750c2e83d39d2f3b68e
enobela <enobela@RCGPPTACA095.vpn.advtech.co.za>
2023-03-03 11:43:16 +0200

    Added a class called Student

---
+++ src/demogithub/Student.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demogithub;
+
+/**
+ *
+ * @author enobela
+ */
+public class Student {
+    String name;
+    String sname;
+    int id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    
+    
+}
